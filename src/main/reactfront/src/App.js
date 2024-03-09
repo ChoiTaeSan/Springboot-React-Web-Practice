0fadd4e2523f46471db3cbe8555c25341bc29427
Choi TaeSan <xotks7524@gmail.com>
2024-03-09 22:29:50 +0900

    Feat: 회원가입 기능 추가

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,15 +1,17 @@
-// App.js
 import React from 'react';
 import Login from './Login'; // Login 컴포넌트를 불러옵니다.
-
+import Signup from './Signup'; // Signup 컴포넌트를 불러옵니다.
 
 function App() {
-  return (
-      <div className="App">
-        <h1>Login Example</h1>
-        <Login />
-      </div>
-  );
+    return (
+        <div className="App">
+            <h1>Login Example</h1>
+            <Login />
+            <hr />
+            <h1>Signup</h1>
+            <Signup />
+        </div>
+    );
 }
 
 export default App;
